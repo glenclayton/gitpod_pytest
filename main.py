a64f1e143b5bdeac2853b3e8aaa2a208628030e6
Glen Clayton <glen.clayton@gmail.com>
2023-10-21 08:51:08 +0000

    update

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import numpy as np
+
 def add(a,b):
     return a+b
+
+def my_concat(a,b):
+    return np.concatenate((a,b))
--- test_main.py
+++ test_main.py
@@ -1,4 +1,12 @@
 import main
+import numpy as np
 
 def test_add():
     assert main.add(1,2) == 3
+
+def test_concat():
+    a = np.array([1,2])
+    b = np.array([3,4])
+    c = main.my_concat(a,b)
+    assert c.size == 4
+
